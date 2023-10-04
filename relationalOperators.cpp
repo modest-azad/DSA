65192915c89dc1b4a6b200e19cf4932e4baaab1e
spotthecode <collezian@gmail.com>
2023-10-04 11:25:35 +0530

    Lecture 2 completed

---
+++ relationalOperators.cpp
@@ -0,0 +1,28 @@
+// #Relational Operators
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 8;
+
+    bool first = (a>b);
+    cout<<first<<endl;
+    
+    bool second = (a<b);
+    cout<<second<<endl;
+
+    bool third = (a>=b);
+    cout<<third<<endl;
+
+    bool forth = (a==b);
+    cout<<forth<<endl;
+
+    bool sixth = (a!=b);
+    cout<<sixth<<endl;
+
+    // NOTES :0 (false), 1(true )
+    
+    return 0;
+}
